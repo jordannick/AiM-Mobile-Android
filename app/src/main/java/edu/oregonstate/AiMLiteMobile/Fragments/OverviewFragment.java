060c4a59f838b57ae1669d9661da7df08d91bf3c
sellers04 <sellers.kevin@gmail.com>
2015-06-02 10:59:15 -0700

    Landing UI tweaks

--- app/src/main/java/edu/oregonstate/AiMLiteMobile/Fragments/OverviewFragment.java
+++ app/src/main/java/edu/oregonstate/AiMLiteMobile/Fragments/OverviewFragment.java
@@ -1,14 +1,14 @@
 package edu.oregonstate.AiMLiteMobile.Fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import edu.oregonstate.AiMLiteMobile.Activities.OverviewListActivity;
 import edu.oregonstate.AiMLiteMobile.R;
 
 /**
@@ -31,13 +31,33 @@ public class OverviewFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.overview_landing, container, false);
 
-        v.findViewById(R.id.button_scrollToDaily).setOnClickListener(new View.OnClickListener() {
+
+        TextView iconDaily = (TextView)v.findViewById(R.id.overviewLanding_daily_icon);
+        TextView iconBacklog = (TextView)v.findViewById(R.id.overviewLanding_backlog_icon);
+        TextView iconAdmin = (TextView)v.findViewById(R.id.overviewLanding_admin_icon);
+        TextView iconRecentlyCompleted = (TextView)v.findViewById(R.id.overviewLanding_recentlyCompleted_icon);
+
+        TextView iconNotices = (TextView)v.findViewById(R.id.overviewLanding_notices_icon);
+        TextView iconTimeLog = (TextView)v.findViewById(R.id.overviewLanding_timeLog_icon);
+
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/FontAwesome.otf");
+        iconDaily.setTypeface(tf); iconBacklog.setTypeface(tf); iconAdmin.setTypeface(tf); iconRecentlyCompleted.setTypeface(tf);
+        iconNotices.setTypeface(tf); iconTimeLog.setTypeface(tf);
+
+        iconDaily.setText(getString(R.string.icon_daily));
+        iconBacklog.setText(getString(R.string.icon_backlog));
+        iconAdmin.setText(getString(R.string.icon_admin));
+        iconRecentlyCompleted.setText(getString(R.string.icon_recentlyCompleted));
+        iconNotices.setText(getString(R.string.icon_notices));
+        iconTimeLog.setText(getString(R.string.icon_timeLog));
+
+/*        v.findViewById(R.id.button_scrollToDaily).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "pressed the button");
-                ((OverviewListActivity)getActivity()).scrollToPosition(8);
+                ((OverviewListActivity) getActivity()).scrollToPosition(8);
             }
-        });
+        });*/
 
         return v;
     }
